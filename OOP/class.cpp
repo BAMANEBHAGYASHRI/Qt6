fa41686f7f38321413b544c87dfd27498bfdf3c3
BAMANEBHAGYASHRI <shrutibamaneb@gmail.com>
2025-09-19 11:34:24 +0530

    Stack allocation And Dynamic allocation

--- OOP/class.cpp
+++ OOP/class.cpp
@@ -5,19 +5,39 @@ using namespace std;
     public:
         int val;
         string surname;
-        string name="bhagyashri";
+        string name;
+    
+        test(){
+            val=0;
+            surname="bamane";
+            name="sonali";
+        }
+        test(int v, string s, string n){
+            val=v;
+            surname=s;
+            name=n;
+        }
 
     void printTest(){
-        cout << val << endl;
-         cout << surname<< endl;
-        cout << name << endl;
+        cout << "value::" << val << endl;
+         cout <<"surname::" << surname<< endl;
+        cout << "name::" <<name << endl;
        
     }
  };
 
  
  int main(){
+    // static allocation
     test t1;
+    t1.val=1;
+    t1.name="bhagyashri";
+    t1.surname="bamane";
+
+    // dynamic allocation
+    test *t2=new test(2,"shruti","bamane");
+    
     t1.printTest();
+    t2->printTest();
     return 0;
  }
